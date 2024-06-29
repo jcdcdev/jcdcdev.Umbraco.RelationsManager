6bddb406d3297c525671d7350c81bd916065fbc0
jcdcdev <james@jcdc.dev>
2024-06-29 12:57:20 +0100

    fix: enable pagination for relations API

--- src/jcdcdev.Umbraco.RelationsManager/Controllers/RelationTypeTreeItemController.cs
+++ src/jcdcdev.Umbraco.RelationsManager/Controllers/RelationTypeTreeItemController.cs
@@ -14,9 +14,9 @@ public class RelationTypeTreeItemController(IRelationService relationService) : 
     [ApiExplorerSettings(GroupName = "Relation Type")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(PagedViewModel<RelationTypeTreeItemResponseModel>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PagedViewModel<RelationTypeTreeItemResponseModel>>> GetChildren(int skip = 0, int take = 100)
+    public async Task<ActionResult<PagedViewModel<RelationTypeTreeItemResponseModel>>> GetChildren(int skip = 0, int take = 999)
     {
-        var relationTypes = RelationService.GetAllRelationTypes().ToList();
+        var relationTypes = RelationService.GetAllRelationTypes().Skip(skip).Take(take).ToList();
         var data = relationTypes.Select(x => new RelationTypeTreeItemResponseModel
         {
             Name = x.Name,
