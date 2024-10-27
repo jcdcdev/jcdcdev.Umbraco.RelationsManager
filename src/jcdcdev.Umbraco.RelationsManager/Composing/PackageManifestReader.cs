8ef41c0cb817a0719cf293c7d02493d7784d9666
jcdcdev <james@jcdc.dev>
2024-10-27 18:37:08 +0000

    refactor: replace umbraco-package.json with PackageManifestReader

---
+++ src/jcdcdev.Umbraco.RelationsManager/Composing/PackageManifestReader.cs
@@ -0,0 +1,32 @@
+using jcdcdev.Umbraco.Core.Extensions;
+using jcdcdev.Umbraco.Core.Web.Models.Manifests;
+using Umbraco.Cms.Core.Manifest;
+using Umbraco.Cms.Infrastructure.Manifest;
+
+namespace jcdcdev.Umbraco.RelationsManager.Composing;
+
+public class PackageManifestReader : IPackageManifestReader
+{
+    public async Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
+    {
+        var extensions = new List<IManifest>();
+        var packageManifest = new PackageManifest
+        {
+            Name = Constants.PackageName,
+            Version = EnvironmentExtensions.CurrentAssemblyVersion().ToSemVer()?.ToString() ?? "0.1.0",
+            AllowPublicAccess = false,
+            AllowTelemetry = true,
+            Extensions = []
+        };
+
+        extensions.Add(new BackofficeEntryPointManifest
+        {
+            Name = "relations-manager.entrypoint",
+            Alias = "relations-manager.entrypoint",
+            Js = "/App_Plugins/RelationsManager/dist/index.js"
+        });
+
+        packageManifest.Extensions = extensions.OfType<object>().ToArray();
+        return [packageManifest];
+    }
+}
